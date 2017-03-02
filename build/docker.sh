8f22665b8045c408d1cdddab77e4fb0232bef9b6
Jon Evans <jon.evans@ixxus.com>
2017-03-02 17:07:07 +0000

    More debugging

--- build/docker.sh
+++ build/docker.sh
@@ -4,8 +4,12 @@ VERSION=$1
 
 ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"
 
-cp "$ROOT/target/*.jar" "$ROOT/docker"
+cd $ROOT
 
-cd "$ROOT/docker"
+echo "in directory $ROOT"
+
+cp target/*.jar docker
+
+cd docker
 docker build -t evansj/test-app:latest -t evansj/test-app:$VERSION .
 
