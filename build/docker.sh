c6eba3c209b4f0cabdf9ce3ea9fdf7c698fc64b3
Jon Evans <jon.evans@ixxus.com>
2017-03-02 16:53:41 +0000

    Fix paths

--- build/docker.sh
+++ build/docker.sh
@@ -3,9 +3,8 @@
 VERSION=$1
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-cd $DIR/..
 
-cp "$DIR/../target/*.jar" "$DIR/docker"
+cp "$DIR/target/*.jar" "$DIR/docker"
 
 cd "$DIR/docker"
 docker build -t evansj/test-app:latest -t evansj/test-app:$VERSION .
